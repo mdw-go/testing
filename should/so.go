1c332dbbe76ce4dae19222c8880c4a5cb43fbf75
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2025-08-09 06:38:10 -0600

    Remove intermediate type.

--- should/so.go
+++ should/so.go
@@ -6,8 +6,6 @@ import (
 	"github.com/mdw-go/testing/v2/suite"
 )
 
-type Func func(actual any, expected ...any) error
-
 func So(t *testing.T, actual any, assertion suite.Func, expected ...any) {
 	t.Helper()
 	_ = suite.New(t).So(actual, assertion, expected...)
